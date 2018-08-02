7a42bf7c8cfbbfb4d18dca74684915a0374e2b1e
Jessica Klein <jess.klein@live.com>
2018-08-02 09:35:10 -0400

    Update randomemail.sql

--- randomemail.sql
+++ randomemail.sql
@@ -1,21 +1,27 @@
 USE [AdventureWorksDW]
 GO
-WITH random as (
-	SELECT TOP 1000
-		c1.[FirstName],
-		c2.[LastName],CAST(RAND(CHECKSUM(NEWID()))*3 as int) randomemail
 
-		FROM [dbo].[DimCustomer] c1
-		CROSS JOIN
-		DimCustomer c2
-	)
-	SELECT Firstname, Lastname, email=
-	CASE
-		when randomemail =0 then
-			lower(left(FirstName,1)+[LastName])+'@hotmail.com'
-		when randomemail =1 then
-			lower(left(FirstName,1)+[LastName])+'@gmail.com'
-		else
-			lower(left(FirstName,1)+[LastName])+'@yahoo.com'
-	END
-FROM random
+WITH random
+as
+(
+SELECT TOP 1000000
+      c1.[FirstName],
+	  c2.[LastName],CAST(RAND(CHECKSUM(NEWID()))*3 as int) randomemail
+ 
+  FROM [dbo].[DimCustomer] c1
+CROSS JOIN
+DimCustomer c2
+)
+select  
+Firstname, 
+Lastname,
+email=
+CASE
+	when randomemail =0 then 
+	lower(left(FirstName,3)+'_'+[LastName])+'@hotmail.com'
+	when randomemail =1 then 
+	lower(left(FirstName,3)+'_'+[LastName])+'@gmail.com'
+	else
+	lower(left(FirstName,3)+'_'+[LastName])+'@yahoo.com'
+END
+from random
